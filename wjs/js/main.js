a9f7de50eb98f5bea2d6d9b8e494e8871f0b4650
unknown <1017370718@qq.com>
2016-09-21 22:26:14 +0800

    添加轮播图

---
+++ wjs/js/main.js
@@ -0,0 +1,41 @@
+/*
+ * 自己的JS脚步
+ * @Author: iceStone
+ * @Date:   2015-12-12 10:59:26
+ * @Last Modified by:   iceStone
+ * @Last Modified time: 2015-12-13 15:19:19
+ */
+
+'use strict';
+
+$(function() {
+    // 当文档加载完成才会执行
+    /**
+     * 根据屏幕宽度的变化决定轮播图片应该展示什么
+     * @return {[type]} [description]
+     */
+    function resize() {
+        // 获取屏幕宽度
+        var windowWidth = $(window).width();
+        // 判断屏幕属于大还是小
+        var isSmallScreen = windowWidth < 768;
+        // 根据大小为界面上的每一张轮播图设置背景
+        // $('#main_ad > .carousel-inner > .item') // 获取到的是一个DOM数组（多个元素）
+        $('#main_ad > .carousel-inner > .item').each(function(i, item) {
+            var $item = $(item);
+            // var imgSrc = $item.data(isSmallScreen ? 'image-xs' : 'image-lg');
+            var imgSrc =
+                isSmallScreen ? $item.data('image-xs') : $item.data('image-lg');
+            // 设置背景图片
+            $item.css('backgroundImage', 'url("' + imgSrc + '")');
+            //
+            // 因为我们需要小图时 尺寸等比例变化，所以小图时我们使用img方式
+            if (isSmallScreen) {
+                $item.html('<img src="' + imgSrc + '" alt="" />');
+            } else {
+                $item.empty();
+            }
+        });
+    }
+    $(window).on('resize', resize).trigger('resize');
+});
